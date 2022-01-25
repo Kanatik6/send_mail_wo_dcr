3178e26b661d194a414133e1ba807d710d636961
Your Name <you@example.com>
2022-01-25 19:28:49 +0600

    add docker-compose

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mail/',include('main_app.urls'))
+    path('mail/',include('main_app.urls')),
+
 ]
